330d90bec42b5d66c97942fe3f4be74b108c2682
tavianot <tavianot@gmail.com>
2017-10-28 23:29:24 -0400

    GA completed also adding pictures of different runs

--- blocks.py
+++ blocks.py
@@ -10,7 +10,8 @@ import copy as c
 import numpy as np
 import matplotlib as mp
 import matplotlib.pyplot as pp
-r.seed(1)
+import matplotlib.ticker as ticker
+#r.seed(107)
 
 #class for blocks within sudoku puzzle
 class Block:
@@ -20,6 +21,11 @@ class Block:
         self.block = [x for x in range(1,self.total +1)]
         r.shuffle(self.block)
     def shuffle(self):
+#        first = r.randint(0,self.width-1)
+#        second = r.randint(0,self.width-1)
+#        temp = c.deepcopy(self.block[first])
+#        self.block[first]= c.deepcopy(self.block[second])
+#        self.block[second]= temp
         r.shuffle(self.block)
     def __str__(self):
         return str(self.block)
@@ -63,8 +69,8 @@ class Puzzle:
             print(self.getRow(each))
 
     def getCol(self,col):
-        b_start = col//3
-        b_col = col %3
+        b_start = col//self.blockwidth
+        b_col = col %self.blockwidth
         temp_col =list()
         for x in range(self.blockwidth):
             temp_col += self.blocks[(x*self.blockwidth)+b_start].getCol(b_col)
@@ -79,26 +85,29 @@ class Puzzle:
         return temp_row.copy()
     def ChangeBlock(self,new_block,index):
         self.blocks[index] = c.deepcopy(new_block)
+    def Shuffle(self):
+        r.shuffle(self.blocks)
 
     def fitness(self):
         self.fit =0
         #calculate how many are wrong in each row and column
         for row in range(self.n):
             currentrow = self.getRow(row)
-            for num in range(1,self.n+1):
-                self.fit += abs(currentrow.count(num)-1)
+            if(len(set(currentrow))==len(currentrow)):
+                self.fit -= 0
+            else:
+                self.fit += len(currentrow)-len(set(currentrow))
         for col in range(self.n):
             currentcol = self.getCol(col)
-            for num in range(1,self.n+1):
-                self.fit += abs(currentcol.count(num)-1)
-        #divide by 2 to cut cut away double counting errors.
-        self.fit = (float(1) /(float(self.fit/2)+1.0))**2
+
+            if(len(set(currentcol))==len(currentcol)):
+                self.fit -= 0
+            else:
+                self.fit += len(currentcol)-len(set(currentcol))
+        
         return self.fit
     def __lt__(self,other):
-        if(self.fit != None and other.fit != None):
-            return self.fit < other.fit
-        else:
-            return self.fitness() < other.fitness()
+        return self.fitness() > other.fitness()
 
 def GeneratePop(pop_size,n):
     population_list = list()
@@ -113,21 +122,23 @@ def TotalFitness(pop_list):
     for each in pop_list:
         total += each.fit
     return total
-def SelectParents(pop_list):
-    total = TotalFitness(pop_list)
+def SelectParents(pop_list,pop_size):
     select_list = list()
-    for each in pop_list:
-        num = round(each.fit/total*100)
-        for x in range(num):
-            select_list.append(c.deepcopy(each))
-    r.shuffle(select_list)
-    return  c.deepcopy(select_list)
+    for each in range(pop_size):
+        first =r.choice(pop_list)
+        second = r.choice(pop_list)
+        if(first.fitness()>= second.fitness()):
+            select_list.append(c.deepcopy(first))
+        else:
+            select_list.append(c.deepcopy(second))
+    return  select_list
 
 def MakeChildren(n,p,parents,crossover,mutation):
         children = list()
         for each in range(p):
                 p1 = c.deepcopy(r.choice(parents))
                 p2 = c.deepcopy(r.choice(parents))
+                
 
                 child1 = c.deepcopy(p1)
                 child2 = c.deepcopy(p2)
@@ -156,41 +167,84 @@ def MakeChildren(n,p,parents,crossover,mutation):
                         #do mutation
                         index = r.randint(0,n-1)
                         child2.blocks[index].shuffle()
+
                 else:
                     pass
                 children.append(c.deepcopy(child1))
                 children.append(c.deepcopy(child2))
         return children
+    
+def MutatePop(pop, mutation):
+    for each in pop:
+        if (r.random() <= mutation):
+            each.Shuffle()
 def test():
-    l= GeneratePop(100,9)
-    SortPop(l)
-    print(l[0].fit)
-    print(l[99].fit)
-    p = SelectParents(l)
-    print(len(p))
-    p[0].print()
-    c = MakeChildren(9,100,p,.4,.3)
-    print(c)
-    c[0].print()
-    SortPop(c)
-    print()
-    c[0].print()
-    print(c[199].fit)
+    pop_size= 600
+    puz_width = 9
+    crossov = .8
+    mutat = .2
+    num_generation =100
+    population = GeneratePop(pop_size,puz_width)
+    SortPop(population)
+    par = SelectParents(population)
+    print(par)
+
     
 def main():
-    pop_size= 1000
+    pop_size= 50 
     puz_width = 9
-    num_generation =10000
-    population = GeneratePop(num_generation,puz_width)
+    crossov = .2
+    mutat = .8
+    pop_mutat = .05
+    num_generation =1000
+    population = GeneratePop(pop_size,puz_width)
     SortPop(population)
     average_fitness = list()
     max_fitness = list()
-    index_list()
+    index_list = list()
     for gen in range(num_generation):
+
         SortPop(population)
-        average_fitness.append(TotalFitness/pop_size)
-        max_fitness.append(population[pop_size-1].fit)
+        average_fitness.append(TotalFitness(population)/pop_size)
+        max_fitness.append(population[pop_size-1].fit)  
         index_list.append(gen)
+        if(population[pop_size-1].fit == 0):
+            break
 
+        if(gen>=num_generation/10):
+             if(len(set(max_fitness[-1*num_generation//10:]))==1):
+                 population = GeneratePop(pop_size,puz_width)
+                 SortPop(population)
 
+        par = SelectParents(population, pop_size)
+        childs = MakeChildren(puz_width,pop_size,par,crossov,mutat)
+        SortPop(childs)
+        temp = c.deepcopy(population) +c.deepcopy(childs)
+        SortPop(temp)
+        MutatePop(temp,pop_mutat)
+        population = c.deepcopy(temp)[-1*pop_size:]
+    print(index_list)
+    print(average_fitness)
+    print(max_fitness)
+    pp.plot(index_list,max_fitness, linewidth=2)
+    pp.plot(index_list,average_fitness,linewidth =2)
+    pp.legend(["best fitness","average fitness"], loc= 'lower left')
+    pp.xlabel('Generation')
+    pp.ylabel('Fitness')
+    pp.title('GA runs with 9x9 puzzles with convergence reset')
+    pp.xticks(index_list)
+    axes = pp.gca()
+    axes.yaxis.set_major_locator(ticker.MultipleLocator(5))
+    #axes.xaxis.set_major_locator(ticker.MultipleLocator(num_generation/10))
+    pp.grid(True)
+    pp.xlim(0,index_list[-1])
+    pp.ylim(0,max(max_fitness+average_fitness))
+    pp.show()
+    
+    
+    
 
+    
+
+#test()
+main()
